f725fda15f4dcbd990eb0eb1b7a8fb074ebb0faa
Nathan Robertus <nathan.robertus@gmail.com>
2018-02-16 23:38:39 -0700

    Added mobile app framework

---
+++ nodeMCU_LANC_APP/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import wifi from 'react-native-android-wifi';
+
+export default class App extends React.Component {
+  render() {
+
+    console.log('here');
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
